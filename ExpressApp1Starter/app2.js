5bc793c4b3633342285f75830a65be4b5a877ec6
Seregfaroth <bl@tokni.com>
2018-01-12 11:52:44 +0000

    untracked app2.js added scripts tp package.json

---
+++ ExpressApp1Starter/app2.js
@@ -0,0 +1,6 @@
+"use strict";
+const ServerController_1 = require("./ServerControl/ServerController");
+var path = "/dev/game";
+var path2 = "/game11";
+var serverController = ServerController_1.default.getInstance(path2);
+//# sourceMappingURL=app2.js.map
