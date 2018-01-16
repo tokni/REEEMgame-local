c76bdc73b9bf25021c83fa677044c4e2af889055
Seregfaroth <bl@tokni.com>
2018-01-16 10:42:49 +0000

    deleted everything

--- ExpressApp1Starter/app2.ts
+++
@@ -1,6 +0,0 @@
-﻿
-import ServerController from './ServerControl/ServerController';
-var path = "/dev/game";
-var path2 = "/game11";
-var serverController: ServerController = ServerController.getInstance(path2);
-    
--- ExpressApp1Starter/public/express-publicDev/clientTypescript/clientModel/ClientProfile.ts
+++
@@ -1,6 +0,0 @@
-﻿export class ClientProfile {
-    public nickName: string;
-    public pin: string;
-    public usertype: "participant" | "facilitator";
-    public currentRole: string;
-}
