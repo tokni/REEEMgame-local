c0809adfb93b2f416f40c0fe3c6a29ee9e9f23bf
Seregfaroth <bl@tokni.com>
2018-01-22 09:19:59 +0000

    change webpack.config

--- REEEMgame/webpack.config.js
+++ REEEMgame/webpack.config.js
@@ -1,4 +1,4 @@
-﻿"use strict";
+"use strict";
 var nodeExternals = require('webpack-node-externals');
 
 module.exports = [{
@@ -40,7 +40,7 @@ module.exports = [{
     //    fs: 'empty'
     //}
 }, {
-        entry: __dirname + "/app2",
+        entry: __dirname + "/app2.ts",
         output: { filename: "./REEEMgame.js" },
     target: 'node',
     externals: [nodeExternals()],
